9eda9dfe183a6e62f8f2784728439602d9f98623
Papaabdoulaye <papaabdoulayepipo@gmail.com>
2024-01-01 19:21:11 +0000

    begin register form

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -1,14 +1,90 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import CardWrapper  from './CardWrapper'
-
+import {useForm} from 'react-hook-form'
+import { LoginSchema } from '@/schemas'
+import { zodResolver } from '@hookform/resolvers/zod'
+import {Form, FormControl,FormItem,FormField,FormLabel,FormMessage, FormDescription} from '@/components/ui/form'
+import { z } from 'zod'
+import { Input } from '../ui/input'
+import { Button } from '../ui/button'
+import { Eye,EyeOff } from 'lucide-react'
+import FormError from '../FormError'
+import FormSuccess from '../FormSuccess'
 const LoginForm = () => {
+    const [showPDW,setshowPDW] = useState<boolean>(false)
+    const form = useForm<z.infer<typeof LoginSchema>>({
+        resolver : zodResolver(LoginSchema),
+        defaultValues : {
+            email : '',
+            password : ''
+        }
+    })
+    const onSubmit = (data : z.infer<typeof LoginSchema>) => {
+        console.log("Data");
+        console.log(data);
+        
+        
+    }
     return (
         <CardWrapper 
         headrLabel='Welcome back' 
         backButtonHref='/auth/register' 
         backButtonLabel="Don't have an account?" 
         showSocial>
-            LoginForm
+            <Form {...form}>
+                <form 
+                onSubmit={form.handleSubmit(onSubmit)}
+                className='space-y-6'
+                >
+                    <div className='space-y-4'>
+                        <FormField 
+                            name='email' 
+                            control={form.control}
+                            render={({field}) => (
+                                <FormItem>
+                                    <FormLabel>
+                                        Email
+                                    </FormLabel>
+                                    <FormControl>
+                                        <Input {...field} type='email' placeholder="john.doe.example.com"/>
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                            />
+                        <FormField 
+                            name='password' 
+                            control={form.control}
+                            render={({field}) => (
+                                <FormItem>
+                                    <FormLabel>
+                                        Password
+                                    </FormLabel>
+                                    <FormControl>
+                                        <div className='flex justify-center items-center space-x-1'>
+                                        <Input {...field} type={showPDW ? 'text' : 'password'} placeholder="******"/>
+                                        <span onClick={() => setshowPDW(prev => !prev)} className='cursor-pointer'>
+                                        {showPDW ? (
+                                        <Eye className='h-4 w-4' />
+                                        ) : (
+                                            <EyeOff className='h-4 w-4' />
+                                        )}
+                                        </span>
+                                        </div>
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                            />
+                    </div>
+                    {/* <FormError message='Something went wrong!' /> */}
+                    {/* <FormSuccess message='Email send.' /> */}
+                    <Button className='w-full'  size={'lg'} variant={'ghost'}>
+                        Login
+                    </Button>
+                </form>
+            </Form>
         </CardWrapper>
     )
 }
---
+++ schemas/index.ts
@@ -0,0 +1,8 @@
+import z from 'zod'
+
+export const LoginSchema = z.object({
+    email : z.string().email({message : 'Email is required.'}),
+    password : z.string().min(4,{
+        message : 'Password is required, must be at least 4 characters.'
+    })
+})
